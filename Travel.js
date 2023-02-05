9474c421c506b3e83eb6a3c3238351c98d6f283f
NAMBI KRISHNA MURTHY <ntrkrish5@gmail.com>
2023-02-05 15:53:56 +0530

    intial commit

---
+++ Travel.js
@@ -0,0 +1,60 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+
+window.onscroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('search-bar-containershow');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('navbaractive');
+
+
+}
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('navbaractive');
+    
+
+});
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('search-bar-containershow');
+    
+
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('login-form-containeractive');
+    
+
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('login-form-containeractive');
+    
+
+});
+  
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click' , ()=>{
+        document.querySelector('.control .active').classList.remove('active')
+        btn.classList.add('active');
+        let src=btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src=src;
+
+    })
+    
+
+    
+
+
+
+
+});
